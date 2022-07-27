563bbfe57b228c649924bd70a9d22af92b5f6434
lermanMax <lermanma@gmail.com>
2022-07-27 20:47:45 +0300

    added tests for all apis

--- api_test.py
+++ api_test.py
@@ -7,27 +7,39 @@ import services.api_config
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('api_test')
 
-btc_coin = Coin('btc')
+btc_coin = Coin(
+    name='btc',
+    alter_names={
+        '1inch': 'wbtc'
+    }
+)
 
-for market in Market.all_markets:
-    try:
-        cup = market.get_cup(
-            coin=btc_coin,
-            base_coin=Market.usdt_coin,
-            depth=10
-        )
-        log.info(f'get_cup() for {market.name} returned something')
-    except Exception:
-        log.error(f'get_cup() for {market.name} does not work')
 
-    try:
-        best_price = market.get_price(
-            coin=btc_coin,
-            base_coin=Market.usdt_coin
-        )
-        log.info(
-            f'get_price() for {market.name} returned: {best_price.best_ask.number}')
-    except Exception:
-        log.error(f'get_price() for {market.name} does not work')
+def test_all_markets():
+    for market in Market.all_markets:
+        try:
+            market.get_cup(
+                coin=btc_coin,
+                base_coin=Market.usdt_coin,
+                depth=10
+            )
+            log.info(f'get_cup() for {market.name} returned something')
+        except Exception:
+            log.error(f'get_cup() for {market.name} does not work')
 
-    print('------')
+        try:
+            best_price = market.get_price(
+                coin=btc_coin,
+                base_coin=Market.usdt_coin
+            )
+            log.info((
+                f'get_price() for {market.name} returned: '
+                f'{best_price.best_ask.number}'))
+        except Exception:
+            log.error(f'get_price() for {market.name} does not work')
+
+        print('------')
+
+
+if __name__ == '__main__':
+    test_all_markets()
