32b8a30182f5cf3cfc16cdedc2515d377d602ad5
Max Lerman <57683212+lermanMax@users.noreply.github.com>
2022-11-18 20:19:54 +0300

    deleted ftx

--- services/api_config.py
+++ services/api_config.py
@@ -17,10 +17,6 @@ from .api_pancakeswap import Pancakeswap
 
 # Initialization all Markets
 Huobi()
-Ftx(
-    api_key='VXCjc-Mc-pF142F92NKtvK1MAey9Iilt3dD176pX',
-    api_secret='Bo9SvZpZotPAyJ5WaPtOi1mepiP3QIXtlvD0imqe'
-)
 
 Kraken()
 ByBit()
